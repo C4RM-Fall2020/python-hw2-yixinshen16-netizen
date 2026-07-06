5cea55b1a5a59e3baef6f25848ef9b2c5eded263
yixinshen16-netizen <yixinshen16@gmail.com>
2026-07-05 21:24:39 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
+    nums = np.arange(start, finish + 1)
+    nums_obj = nums.astype(object)
+
+    v = np.where(nums % 15 == 0, 'fizzbuzz',
+        np.where(nums % 3 == 0, 'fizz',
+        np.where(nums % 5 == 0, 'buzz', nums_obj))).tolist()
     return(v)
