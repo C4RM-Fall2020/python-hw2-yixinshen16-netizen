17cae000d2d7a29adacf9465e68b2bef9d96067a
yixinshen16-netizen <yixinshen16@gmail.com>
2026-07-05 21:24:12 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,16 @@
 import numpy as np
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    n = m * ppy
+    r = y / ppy
+    c = face * couponRate / ppy
+
+    t = np.arange(1, n + 1)
+    cf = np.full(n, c)
+    cf[-1] = cf[-1] + face
+
+    pv = cf / (1 + r) ** t
+    price = np.sum(pv)
+
+    x = np.sum((t / ppy) * pv) / price
     return(x)
